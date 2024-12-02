5cd5dd3afbead82397e8ef46b5b70a869dc407d3
Madi <nadikz4444@gmail.com>
2024-12-02 23:05:54 +0500

    СÑоздал проект

---
+++ BackEnd/src/test/java/com/example/quryltai/QuryltaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quryltai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuryltaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
