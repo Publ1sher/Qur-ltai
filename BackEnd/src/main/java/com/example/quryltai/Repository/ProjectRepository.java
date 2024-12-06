d8bed1b78ee71e708bba209d260623449b139d65
Rassul <rasa.laik@gmail.com>
2024-12-06 21:58:45 +0500

    Jpa changed the model referencing to RIGHT model

--- BackEnd/src/main/java/com/example/quryltai/Repository/ProjectRepository.java
+++ BackEnd/src/main/java/com/example/quryltai/Repository/ProjectRepository.java
@@ -1,8 +1,9 @@
 package com.example.quryltai.Repository;
 
+import com.example.quryltai.Model.Projects;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+public interface ProjectRepository extends JpaRepository<Projects, Long> {
 }
