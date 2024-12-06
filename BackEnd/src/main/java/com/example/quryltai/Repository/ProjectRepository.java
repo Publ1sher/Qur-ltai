393611802bc6092e1f8bcd7d1640349a02dcf5c7
Rassul <rasa.laik@gmail.com>
2024-12-06 21:50:06 +0500

    deleting the Project from madi

--- BackEnd/src/main/java/com/example/quryltai/Repository/ProjectRepository.java
+++ BackEnd/src/main/java/com/example/quryltai/Repository/ProjectRepository.java
@@ -1,6 +1,5 @@
 package com.example.quryltai.Repository;
 
-import com.example.quryltai.Model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
